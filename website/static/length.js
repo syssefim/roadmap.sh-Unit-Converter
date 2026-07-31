2aa159b1b5264df5b7b68fda2c76ca4b40ad909e
syssefim <serafim.sharkov@gmail.com>
2026-07-30 23:23:39 -0700

    Implemented length conversion map

--- website/static/length.js
+++ website/static/length.js
@@ -1,5 +1,89 @@
 // length.js
 
+// Create conversion value map
+const conversion = {
+  // mm
+  'mm-mm': 1,
+  'mm-cm': 0.1,
+  'mm-m': 0.001,
+  'mm-km': 0.000001,
+  'mm-in': 0.0393701,
+  'mm-ft': 0.00328084,
+  'mm-yd': 0.00109361,
+  'mm-mi': 0.000000621371,
+
+  // cm
+  'cm-mm': 10,
+  'cm-cm': 1,
+  'cm-m': 0.01,
+  'cm-km': 0.00001,
+  'cm-in': 0.393701,
+  'cm-ft': 0.0328084,
+  'cm-yd': 0.0109361,
+  'cm-mi': 0.00000621371,
+
+  // m
+  'm-mm': 1000,
+  'm-cm': 100,
+  'm-m': 1,
+  'm-km': 0.001,
+  'm-in': 39.3701,
+  'm-ft': 3.28084,
+  'm-yd': 1.09361,
+  'm-mi': 0.000621371,
+
+  // km
+  'km-mm': 1000000,
+  'km-cm': 100000,
+  'km-m': 1000,
+  'km-km': 1,
+  'km-in': 39370.1,
+  'km-ft': 3280.84,
+  'km-yd': 1093.61,
+  'km-mi': 0.621371,
+
+  // in
+  'in-mm': 25.4,
+  'in-cm': 2.54,
+  'in-m': 0.0254,
+  'in-km': 0.0000254,
+  'in-in': 1,
+  'in-ft': 0.0833333,
+  'in-yd': 0.0277778,
+  'in-mi': 0.0000157828,
+
+  // ft
+  'ft-mm': 304.8,
+  'ft-cm': 30.48,
+  'ft-m': 0.3048,
+  'ft-km': 0.0003048,
+  'ft-in': 12,
+  'ft-ft': 1,
+  'ft-yd': 0.333333,
+  'ft-mi': 0.000189394,
+
+  // yd
+  'yd-mm': 914.4,
+  'yd-cm': 91.44,
+  'yd-m': 0.9144,
+  'yd-km': 0.0009144,
+  'yd-in': 36,
+  'yd-ft': 3,
+  'yd-yd': 1,
+  'yd-mi': 0.000568182,
+
+  // mi
+  'mi-mm': 1609344,
+  'mi-cm': 160934.4,
+  'mi-m': 1609.344,
+  'mi-km': 1.609344,
+  'mi-in': 63360,
+  'mi-ft': 5280,
+  'mi-yd': 1760,
+  'mi-mi': 1
+};
+
+
 // Get elements
 const navWeight = document.getElementById("navWeight");
 const navTemperature = document.getElementById("navTemperature");                
@@ -29,13 +113,11 @@ convertButton.addEventListener("click", function() {
     document.getElementById('converted-view').classList.remove('hidden');
 
 
-    // Conversion calculation
-    let result = 0;
+    console.log("Available keys:", Object.keys(conversion));
 
-    if (convertFromUnit.value === 'm' && convertToUnit.value === 'km') {
-        result = length.value / 1000;
-        resultContainer.innerHTML += length.value + ' ' + convertFromUnit.value + ' = ' + result + ' ' + convertToUnit.value;
-    }
+    // Conversion calculation
+    let result = Number(length.value) * conversion[convertFromUnit.value.toLowerCase() + '-' + convertToUnit.value.toLowerCase()];
+    resultContainer.innerHTML += length.value + ' ' + convertFromUnit.value + ' = ' + result + ' ' + convertToUnit.value;
 });
 
 resetButton.addEventListener("click", function() {
